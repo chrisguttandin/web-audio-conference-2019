2b49148895dc25980a7c1556b18472a9ab91c543
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-11-24 04:06:34 +0100

    test: update e2e tests

--- test/e2e/home.po.ts
+++ test/e2e/home.po.ts
@@ -1,6 +1,5 @@
 import { env } from 'process';
 import { browser, by, element } from 'protractor';
-import { promise } from 'selenium-webdriver'; // tslint:disable-line:no-implicit-dependencies
 
 export class HomePage {
     public async getHeadline(): Promise<string> {
@@ -10,11 +9,11 @@ export class HomePage {
         return innerHtml.replace(/<span[^>]*>/g, ' ').replace(/<\/span>/g, '');
     }
 
-    public getSubHeadline(): promise.Promise<string> {
+    public async getSubHeadline(): Promise<string> {
         return element(by.css('wac-app h2')).getText();
     }
 
-    public navigateTo(): promise.Promise<any> {
+    public async navigateTo(): Promise<any> {
         return browser.get(env.IS_SMOKE_TEST === 'true' ? '/web-audio-conference-2019' : '/');
     }
 }
