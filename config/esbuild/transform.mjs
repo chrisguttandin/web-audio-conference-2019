df4566843e3ed655acf09a2095587228b53d1519
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-11-22 21:33:00 +0100

    build(config): keep comments in body

--- config/esbuild/transform.mjs
+++ config/esbuild/transform.mjs
@@ -1,10 +1,17 @@
 import { minify } from 'html-minifier';
+import { JSDOM } from 'jsdom';
 
-export default (indexHtml) =>
-    minify(indexHtml, {
+export default (indexHtml) => {
+    const jsdom = new JSDOM(indexHtml);
+    const options = {
         caseSensitive: true,
         collapseBooleanAttributes: true,
         collapseWhitespace: true,
         minifyCSS: true,
         removeComments: true
-    });
+    };
+
+    jsdom.window.document.head.innerHTML = minify(jsdom.window.document.head.innerHTML, { ...options, removeComments: true });
+
+    return minify(jsdom.serialize(), { ...options, removeComments: false });
+};
